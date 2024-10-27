d2eaedda4a97d6d1c5c19d556d9193b03af38f21
Alexxe <alexxe@fi.unju.edu.ar>
2024-10-27 16:57:22 -0300

    añadiendo la estructura de index.html (navegacion, secciones, footer)

--- devs_proyect/core/views.py
+++ devs_proyect/core/views.py
@@ -2,3 +2,5 @@ from django.shortcuts import HttpResponse,render
 
 
 # Create your views here.
+def index(request):
+    return render(request,"core/index.html")
--- devs_proyect/devs_proyect/urls.py
+++ devs_proyect/devs_proyect/urls.py
@@ -14,12 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from core import views
 from django.contrib import admin
 from django.urls import path
 from core import views
 
 
 urlpatterns = [
+    path('',views.index),
     path('admin/', admin.site.urls),
 ]
 
